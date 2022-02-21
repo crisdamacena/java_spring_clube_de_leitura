083d4a84f7b36b907ab77822d48c34e7a06b0aab
Cristiane Damacena <31530506+crisdamacena@users.noreply.github.com>
2022-02-21 17:45:48 -0300

    Add files via upload

---
+++ springclubedeleitura/springclubedeleitura/src/main/java/com/clubedeleitura/springclubedeleitura/SpringclubedeleituraApplication.java
@@ -0,0 +1,13 @@
+package com.clubedeleitura.springclubedeleitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringclubedeleituraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringclubedeleituraApplication.class, args);
+	}
+
+}
---
+++ springclubedeleitura/springclubedeleitura/src/test/java/com/clubedeleitura/springclubedeleitura/SpringclubedeleituraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubedeleitura.springclubedeleitura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringclubedeleituraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
